9f7e7ac2321e418741b144c7a37a5f706722e5de
David Arroyo Menéndez <d.arroyome@alumnos.urjc.es>
2019-03-18 08:51:49 +0100

    setup.py: add data_files

--- setup.py
+++ setup.py
@@ -21,14 +21,46 @@
 # the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
 # Boston, MA 02110-1301 USA,
 
+import os
+import re
+
 from setuptools import setup
 
 # def readme():
 #     with open('README.org') as f:
 #         return f.read()
 
+cwd = os.getcwd()
+
+def files_one_level(directory):
+    f = os.popen('find '+ directory )
+    l = []
+    for line in f:
+        fields = line.strip().split()
+        l.append(fields[0])
+    return l
+
+def files_one_level_drop_pwd(directory):
+    f = os.popen('find '+ directory)
+    l = []
+    for line in f:
+        fields = line.strip().split()
+        if not(os.path.isdir(fields[0])) and ("__init__.py" not in fields[0]):
+            l.append(drop_pwd(fields[0]))
+    return l
+
+def drop_pwd(s):
+    cwd = os.getcwd()
+    result = ""
+    if re.search(cwd, s):
+        result = re.sub(cwd+'/', '', s)
+    return result
+
+print(files_one_level_drop_pwd(cwd+"/damepandas/files"))
+
+
 setup(name='damepandas',
-      version='0.0.1',
+      version='0.0.2',
       description='Learning Pandas from Tests by David Arroyo Menéndez',
 #      long_description=readme(),
       classifiers=[
@@ -45,6 +77,7 @@ setup(name='damepandas',
       license='GPLv3',
       packages=['damepandas', 'damepandas.tests'],
       package_dir={'damepandas': 'damepandas', 'damepandas.tests': 'damepandas/tests'},
+      data_files=[('damepandas', files_one_level_drop_pwd(cwd+"/damepandas/files"))],      
       install_requires=[
           'markdown',
           'numpy',
