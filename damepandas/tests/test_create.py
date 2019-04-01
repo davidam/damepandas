38c490de956c6bda6d02c48c5f4e1753aa2312b9
David Arroyo Menéndez <d.arroyome@alumnos.urjc.es>
2019-04-01 14:55:16 +0200

    tests/test_create.py: add serialization

--- damepandas/tests/test_create.py
+++ damepandas/tests/test_create.py
@@ -31,17 +31,19 @@ class TestCreate(TestCase):
     def test_series(self):
         s1 = pd.Series(['a', 'b'])
         self.assertEqual(len(s1), 2)
+        self.assertEqual('0    a\n1    b\ndtype: object', repr(s1))
 
     def test_dataframe(self):
         df2 = pd.DataFrame([['c', 3], ['d', 4]], columns=['letter', 'number'])
         self.assertTrue(len(df2) > 0)
-        
+        self.assertEqual('  letter  number\n0      c       3\n1      d       4', repr(df2))
+
     def test_range(self):
         dates = pd.date_range('20130101', periods=6)
+        self.assertEqual("DatetimeIndex(['2013-01-01', '2013-01-02', '2013-01-03', '2013-01-04',\n               '2013-01-05', '2013-01-06'],\n              dtype='datetime64[ns]', freq='D')", repr(dates))
         self.assertTrue(len(dates) > 0)
 
     def test_fill_random(self):
         dates = pd.date_range('20130101', periods=6)
         df = pd.DataFrame(np.random.randn(6,4), index=dates, columns=list('ABCD'))
         self.assertTrue(len(df) > 0)
-
