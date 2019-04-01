874d8da525fca4f0a81561c25f4117a1fafdd29b
David Arroyo Menéndez <d.arroyome@alumnos.urjc.es>
2019-04-01 17:27:15 +0200

    tests/test_create.py: add from_dict

--- damepandas/tests/test_create.py
+++ damepandas/tests/test_create.py
@@ -38,6 +38,11 @@ class TestCreate(TestCase):
         self.assertTrue(len(df2) > 0)
         self.assertEqual('  letter  number\n0      c       3\n1      d       4', repr(df2))
 
+    def test_dataframe_from_dict(self):
+        pdffd = pd.DataFrame.from_dict(dict([('A', [1, 2, 3]), ('B', [4, 5, 6])]))
+        self.assertEqual(len(pdffd), 3)
+        self.assertEqual('   A  B\n0  1  4\n1  2  5\n2  3  6', repr(pdffd))
+
     def test_range(self):
         dates = pd.date_range('20130101', periods=6)
         self.assertEqual("DatetimeIndex(['2013-01-01', '2013-01-02', '2013-01-03', '2013-01-04',\n               '2013-01-05', '2013-01-06'],\n              dtype='datetime64[ns]', freq='D')", repr(dates))
